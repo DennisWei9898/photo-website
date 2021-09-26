4d6fc661054745a81dc9d73c8f90e625039373a8
DennisWei9898 <denniseasycard@gmail.com>
2021-09-26 22:57:58 +0800

    feat: picture search website

---
+++ src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://www.cakeresume.com/dennis-wei-5021c1"
+          >
+            About
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
